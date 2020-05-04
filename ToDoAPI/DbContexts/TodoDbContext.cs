e093d14b56941f016ddf0e623031eacc7a1c9b3b
ehievien <ehinomen.evien@gmail.com>
2020-05-03 18:30:20 -0600

    adding dbcontext

---
+++ ToDoAPI/DbContexts/TodoDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ToDoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ToDoAPI.DbContexts
+{
+    public class TodoDbContext : DbContext
+    {
+
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // seed the database with dummy data
+            modelBuilder.Entity<Todo>().HasData(
+                new Todo()
+                {
+                    Id = 1,
+                    Title = "Learn how to cook",
+                    DateCreated = new DateTime(2020, 3, 23),
+                    LastDateUpdated = new DateTime(2020, 4, 23),
+                    Description = "Cook",
+                    Completed = true
+                },
+                new Todo()
+                {
+                    Id = 1,
+                    Title = "Learn how to dance",
+                    DateCreated = new DateTime(2020, 2, 17),
+                    LastDateUpdated = new DateTime(2020, 3, 15),
+                    Description = "Dance",
+                    Completed = true
+                },
+                new Todo()
+                {
+                    Id = 1,
+                    Title = "Learn how to drive",
+                    DateCreated = new DateTime(2020, 3, 15),
+                    LastDateUpdated = new DateTime(2020, 3, 15),
+                    Description = "Drive",
+                    Completed = true
+                }
+                );
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- ToDoAPI/Startup.cs
+++ ToDoAPI/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ToDoAPI.DbContexts;
+
 
 namespace ToDoAPI
 {
@@ -14,8 +18,24 @@ namespace ToDoAPI
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<TodoDbContext>(options =>
+            {
+                options.UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=ToDoDB;Trusted_Connection=True;");
+               // options.UseSqlServer(Configuration["ConnectionString:ToDoDB"]);
+            });
+
             services.AddControllers();
         }
 
